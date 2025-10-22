373e5535b58b44851a20aea1b2ff33046cb3e943
Gustavo Deves Bueno <gdevesbueno02@gmail.com>
2025-10-21 23:44:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // --- 2. Defina o endereço da sua API (o servidor Flask) ---
     // Ele estará rodando localmente na porta 5000
-    const API_URL = "https://dashboard-vendas-mxwr.onrender.com";
+    const API_URL = "https://bot-vendas-api.onrender.com/api/gerar-insights";
 
     /**
      * Adiciona um "ouvinte" ao formulário.
@@ -82,4 +82,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+
 
